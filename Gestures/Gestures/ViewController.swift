98389a6308ba2f96b22c72a21770a415e402c4ff
Rodion Chykerenda <radik.chik@gmail.com>
2021-03-12 12:47:30 +0200

    Gestures Added

---
+++ Gestures/Gestures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gestures
+//
+//  Created by Rodion Chikerenda on 12.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
